108e485e26f5a348ccb587564039dcb22775f52c
Muhammedshifin <muhammedshifink4@gmail.com>
2023-03-16 16:04:04 +0530

    Updated Restart Format 24 To 12 Hrs

--- bot.py
+++ bot.py
@@ -51,7 +51,7 @@ class Bot(Client):
         tz = pytz.timezone('Asia/Kolkata')
         today = date.today()
         now = datetime.now(tz)
-        time = now.strftime("%H:%M:%S %p")
+        time = now.strftime("%I:%M:%S %p")
         await self.send_message(chat_id=LOG_CHANNEL, text=script.RESTART_TXT.format(today, time))
         app = web.AppRunner(await web_server())
         await app.setup()
