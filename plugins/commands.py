b9ce91f867be4cf09ac1bfcd47974f3a85851792
Matrix TG <108185896+matrixbotz0@users.noreply.github.com>
2023-03-25 21:36:40 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -217,7 +217,6 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
-        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('❤️‍🔥 ᴊᴏɪɴ ᴛᴏ ᴄʜᴀɴɴᴇʟ ❤️‍🔥', url=(MAIN_CHANNEL)) ] ] ),
         protect_content=True if pre == 'filep' else False,
         )
                     
