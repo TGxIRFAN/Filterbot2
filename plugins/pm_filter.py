4b0456da2c3ddbbad12fdc26f1406545fe674fbf
AS TG[ONLINE] <126592884+alappu280@users.noreply.github.com>
2023-05-09 21:12:25 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -58,23 +58,95 @@ async def fil_mod(client, message):
       else:
           await m.edit("𝚄𝚂𝙴 :- /autofilter on 𝙾𝚁 /autofilter off")
 
-@Client.on_message((filters.group) & filters.text & filters.incoming)
-async def give_filter(client, message):
-    if message.chat.id != SUPPORT_CHAT_ID:
-        glob = await global_filters(client, message)
-        if glob == False:
-            manual = await manual_filters(client, message)
-            if manual == False:
-                settings = await get_settings(message.chat.id)
+@Client.on_message(filters.group & filters.text & ~filters.edited & filters.incoming)
+
+async def give_filter(client,message):
+
+    group_id = message.chat.id
+
+    name = message.text
+
+    keywords = await get_filters(group_id)
+
+    for keyword in reversed(sorted(keywords, key=len)):
+
+        pattern = r"( |^|[^\w])" + re.escape(keyword) + r"( |$|[^\w])"
+
+        if re.search(pattern, name, flags=re.IGNORECASE):
+
+            reply_text, btn, alert, fileid = await find_filter(group_id, keyword)
+
+            if reply_text:
+
+                reply_text = reply_text.replace("\\n", "\n").replace("\\t", "\t")
+
+            if btn is not None:
+
                 try:
-                    if settings['auto_ffilter']:
-                        await auto_filter(client, message)
-                except KeyError:
-                    grpid = await active_connection(str(message.from_user.id))
-                    await save_group_settings(grpid, 'auto_ffilter', True)
-                    settings = await get_settings(message.chat.id)
-                    if settings['auto_ffilter']:
-                        await auto_filter(client, message)
+
+                    if fileid == "None":
+
+                        if btn == "[]":
+
+                            await message.reply_text(reply_text, disable_web_page_preview=True)
+
+                        else:
+
+                            button = eval(btn)
+
+                            await message.reply_text(
+
+                                reply_text,
+
+                                disable_web_page_preview=True,
+
+                                reply_markup=InlineKeyboardMarkup(button)
+
+                            )
+
+                    elif btn == "[]":
+
+                        await message.reply_cached_media(
+
+                            fileid,
+
+                            caption=reply_text or ""
+
+                        )
+
+                    else:
+
+                        button = eval(btn) 
+
+                        await message.reply_cached_media(
+
+                            fileid,
+
+                            caption=reply_text or "",
+
+                            reply_markup=InlineKeyboardMarkup(button)
+
+                        )
+
+                except Exception as e:
+
+                    logger.exception(e)
+
+                break 
+
+    else:
+
+        await auto_filter(client, message)
+
+        return await asyncio.sleep(10)
+    
+        
+
+  
+                    
+                       
+                
+
 
 @Client.on_message(filters.private & filters.text & filters.incoming)
 async def pm_text(bot, message):
