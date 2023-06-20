d04e4f121c45872dec80cd954ba4f8cb94bf3b2c
Matrix TG <108185896+TGxMATRIX@users.noreply.github.com>
2023-06-20 15:56:59 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1470,6 +1470,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
 async def auto_filter(client, msg, spoll=False):
     reqstr1 = msg.from_user.id if msg.from_user else 0
     reqstr = await client.get_users(reqstr1)
+    message = msg
+    key = f"{message.id}"
     if not spoll:
         message = msg
         settings = await get_settings(message.chat.id)
