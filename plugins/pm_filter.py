1a1f13cd919f47539af3409ec6fe96503a6f4919
Matrix TG <108185896+TGxMATRIX@users.noreply.github.com>
2023-08-06 17:00:09 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -5,6 +5,7 @@ import re
 import ast
 import math
 import random
+lock = asyncio.Lock()
 from pyrogram.errors.exceptions.bad_request_400 import MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty
 from Script import script
 import pyrogram
